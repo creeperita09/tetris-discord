74295b731ad8f02e3625b7c38fe4f64bff2337f7
creeperita09 <97898994+creeperita09@users.noreply.github.com>
2025-07-14 18:47:57 +0200

    Delete test_game.js

--- test_game.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const runTetrisBot = require('./game');
-
-(async () => {
-  const buffer = await runTetrisBot(['a', 's', 'd', 'w', 'z', 'x', 'a']);
-  fs.writeFileSync('animated_output.webp', buffer);
-  console.log('Saved animated_output.webp');
-})();
